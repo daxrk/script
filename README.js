8eebe3ad155e062034dc4b821f313566278d6eed
mostafa6679 <77597243+mostafa6679@users.noreply.github.com>
2021-03-31 22:33:04 +0200

    Update and rename README.md to README.js

---
+++ README.js
@@ -0,0 +1,81 @@
+$("#actor1").click(function(){
+    var nameofactor1 = $(".nameofActor1").val();
+    var nameofactor2 = $(".nameofActor2").val();
+    var nameofactor3 = $(".nameofActor3").val();
+    var nameofactor4 = $(".nameofActor4").val();
+    var nameofactor5 = $(".nameofActor5").val();
+    var story = $(".story").val();
+    var urle = $(".url").val();
+    var youtube = $(".youtube").val();
+    var rate = $(".rate").val();
+    var Type = $(".Type").val();
+    var Type1 = $(".Type1").val();
+    var Type2 = $(".Type2").val();
+    var time = $(".time").val();
+    var qualte = $(".qualte").val();
+    var tito = $(".tito").val();
+    var year = $(".year").val();
+    var photo = $(".photo").val();
+    var actor = "<div class='post-body entry-content entry-summary notr' style='text-align: center;'><link href='https://raw.githack.com/mostafa6679/cimastoreserver/main/cima-stores.css' rel='stylesheet'><div class='title293'><div class='separator' style='clear: both; text-align: center;'></div><div></div><div class='title70'>" + nameofactor1 + "</div></div></div>
+<div class='hjsl' id='up'>
+</div><b>  <div><div class='sdf'>
+<div class='fhsk' style='margin:0'> <kl id='menu' style='text-align: center;'>
+<li onclick="myframe.location.href='https://cimanow-tv-o-4g-movizland-online.blogspot.com/2021/03/blog-post_31.html'" class="sq-button xl bb">moshahda<span class="hd">متعدد الجودات</span></li>
+<li onclick="myframe.location.href='//www.ok.ru/videoembed/2043712703071'" class="sq-button xl">ok<span class="hd">متعدد الجودات</span></li>
+<li onclick="myframe.location.href='https://vidbeem.com/embed-pu2gcvg3j6yd.html'" class="sq-button xl">vidbem<span class="hd">720p</span></li> 
+<li onclick="myframe.location.href='https://vidshaar.com/embed-vh0kaasbmcg6.html'" class="sq-button xl">vidshar<span class="hd">720p</span></li>
+<li onclick="myframe.location.href='https://uqload.com/embed-f5jli26r8wfy.html'" class="sq-button xl">uqload<span class="hd">720p</span></li>
+</kl> </div>
+  <article> <h2 style="margin: 0;">   <div class="lldk" align="center">
+</div>        
+<a href="#" class="night-overlay"></a>
+          <div class="media-stream">
+      <div class="col-12 col-l-6 stream-player">
+    <a href="#" class="night-mode ti-lightbulb-ios tooltip" data-title="الوضع اليلي"></a>
+<iframe allow="accelerometer;encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" marginheight="0" marginwidth="0" name="myframe" scrolling="no" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen" sandbox="allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation" frameborder="0"  src="https://cimanow-tv-o-4g-movizland-online.blogspot.com/2021/03/blog-post_31.html" width="100%" height="420" frameborder="0"></iframe></div> <div class="clr"></div></div></h2></article>
+
+<script>
+  var items = document.getElementById("menu").getElementsByTagName("li");
+ var hja = document.getElementById("text");
+   var mkkl = document.getElementById("menu");
+  
+  for (i = 0; i < items.length; i++) {
+  items[i].addEventListener("click",schh);}
+  
+  function schh(){
+      for (i = 0; i < items.length; i++) {
+    items[i].classList.remove("bb");
+      }
+        this.classList.add("bb")
+    
+  }
+  </script> </div>
+
+          <h1 class="kldh"><div class="jks"><span> روابط تحميل مباشرة:</span></div> <div style="padding: 5px;">
+<a href="https://4g-movizland-online.blogspot.com/2021/03/godzilla-vs-kong-2021.html" class="sq-button lg"><i class="fa fa-download" style="margin: 0;"></i> الانتقال لصفحة التحميل</a></div>
+</h1>
+               
+<h1 class="kldh"><div class="jks"><span> روابط تحميل:</span></div> <div style="padding: 5px;">
+     <table id="DownloadTable" ><thead><tr><th>الجودة</th><th>الدقة</th><th>النوع</th><th style="width: 200px;">الرابط</th></tr></thead><tbody><tr><td>HD</td><td>متعدد الجودات</td><td>fembed</td><td><a href="https://www.fembed.com/f/plzdltm8zjexkxm" target="_blank"><i class="fa fa-download"></i>تحميل الان</a></td></tr></tbody></table>
+        <table id="DownloadTable" ><thead><tr><th>الجودة</th><th>الدقة</th><th>النوع</th><th style="width: 200px;">الرابط</th></tr></thead><tbody><tr><td>HD</td><td>720p</td><td>sama-share</td><td><a href="https://sama-share.com/zx6npsuqwm82" target="_blank"><i class="fa fa-download"></i>تحميل الان</a></td></tr></tbody></table>
+          <table id="DownloadTable" ><thead><tr><th>الجودة</th><th>الدقة</th><th>النوع</th><th style="width: 200px;">الرابط</th></tr></thead><tbody><tr><td>HD</td><td>720p</td><td>mdiaload</td><td><a href="https://down.mdiaload.com/nx9c64gowqkg" target="_blank"><i class="fa fa-download"></i>تحميل الان</a></td></tr></tbody></table>
+
+        </div>       
+  </h1><div class="clear"></div>
+  <div class="sq-widepost"></div>
+    <style>
+         .side-right{
+      
+      float: none;
+    margin: auto;
+      
+      }   
+  </style>  <script src='https://shahid4u.onl/themes/Shahid4u/js/tornado.min.js?v=1.1.8'></script>";
+    $("textarea").val(actor).select();
+    document.execCommand("copy");
+    $("#actor1").text("انتظر")
+    setTimeout(function(){
+        $("#actor1").text("تم النسخ");
+    },2000);
+    $(".nameOfActor").val("");
+  })
